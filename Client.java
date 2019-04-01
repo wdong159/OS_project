88392dc40883cb8a55a8c0048c09414ffa914ffe
Earlusionist <marko_XLR8@hotmail.com>
2019-04-01 00:26:17 -0400

    Editied comments and cleaned up output.

--- Client.java
+++ Client.java
@@ -1,114 +1,108 @@
+
 /*
-course number (CS4345)
-Semester/Year (Spring 2019)
-assignment identifier (Assignment 3)
-all group members’ names (Wei Dong, Guanghui Li, Marco Colasito) 
+Course Number: CS4345
+Spring 2019
+Assignment 3
+Members: Wei Dong, Guanghui Li, Marco Colasito
 */
-
-
 
 import java.net.*;
 import java.io.*;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-//import com.sun.org.apache.xerces.internal.impl.xs.identity.Selector.Matcher;
 
-public class Client{
+public class Client {
 	static String name;
 	static boolean go = false;
-	public static void main(String[] args) throws Exception{
-		try{
-			//create a socket to make connection to server socket
+
+	public static void main(String[] args) throws Exception {
+		try {
+			// create a socket to make a connection to the server socket
 			Socket sock = new Socket("127.0.0.1", 7000);
-			Scanner scn = new Scanner (System.in);
-			Scanner write = new Scanner (System.in);
-		
-			//create an output stream to send data to the server
+			Scanner scn = new Scanner(System.in);
+			Scanner write = new Scanner(System.in);
+
+			// create an output stream to send data to the server
 			DataOutputStream data2server = new DataOutputStream(sock.getOutputStream());
-			//create an input stream to receive data from server
+			// create an input stream to receive data from the server
 			DataInputStream result4mserver = new DataInputStream(sock.getInputStream());
-			
 
 			/*
-			thread for  sending out message to the server
-			*/
-			Thread sendMsg = new Thread(new Runnable(){
-			
+			 * thread for sending out messages to the server
+			 */
+			Thread sendMsg = new Thread(new Runnable() {
+
 				@Override
 				public void run() {
-					while(true){
-							String msg = write.nextLine();
-							try{
-								if(msg.equals("LOGOUT")){
-									data2server.writeUTF(msg);
-									break;
-								}
+					while (true) {
+						String msg = write.nextLine();
+						try {
+							if (msg.equals("LOGOUT")) {
 								data2server.writeUTF(msg);
-							}catch(Exception e){
-								System.out.println(e);
 								break;
 							}
+							data2server.writeUTF(msg);
+						} catch (Exception e) {
+							System.out.println(e);
+							break;
+						}
 					}
 				}
 			});
 
-
 			/*
-			thread for reading message from the client
-			*/
-			Thread readMsg = new Thread (new Runnable(){
-			
+			 * thread for reading messages from the client
+			 */
+			Thread readMsg = new Thread(new Runnable() {
+
 				@Override
 				public void run() {
-					try{
-						while(true){
-							try{
+					try {
+						while (true) {
+							try {
 								/*
-								read the output from the server
-								take action base on what server had send
-								for example when you join the server will ask for user name
-								*/
+								 * read the output from the server and take action based on what server had asked for
+								 * Ex: when you join, the server will ask for username
+								 */
 								String result = result4mserver.readUTF();
-								if(result.startsWith("YOURNAME")){
-										System.out.println(result);
-										name = scn.next();
-										name.toUpperCase();
-										data2server.writeUTF(name);
-										sendMsg.start();
-								}else if(result.startsWith("Message")){
+								if (result.startsWith("YOURNAME")) {
+									System.out.println(result);
+									name = scn.next();
+									name.toUpperCase();
+									data2server.writeUTF(name);
+									sendMsg.start();
+								} else if (result.startsWith("Message")) {
 									Pattern pattern = Pattern.compile(name);
 									Matcher there = pattern.matcher(result);
-									if(!there.find())
+									if (!there.find())
 										System.out.println(result.substring(7));
-								}else if(result.startsWith("JOINGROUP")){
-									System.out.println(result.substring(9) + " has join the chat");
-								}else if(result.equals("GOCHAT!")){
-										System.out.println("You can start talking now!");
-								}else if(result.equals("YOUHAVELOGOUT")){
-									System.out.println("YOU had log out from the chat");
+								} else if (result.startsWith("JOINGROUP")) {
+									System.out.println(result.substring(9) + " has joined the chat\n");
+								} else if (result.equals("GOCHAT!")) {
+									System.out.println("You can start talking now!\n");
+								} else if (result.equals("YOUHAVELOGOUT")) {
+									System.out.println("YOU has left the chat\n");
 									break;
-								}else{
 								}
-							}catch (Exception e){
+							} catch (Exception e) {
 								System.out.println(e);
 								break;
 							}
 						}
-					}catch (Exception e){
-								
-						System.out.println("Server coonection has failed");
+					} catch (Exception e) {
+
+						System.out.println("Server connection has failed");
 					}
 				}
 			});
-			
 
 			readMsg.start();
-			//sendMsg.start();
-			
-		 } catch(IOException ioe){
-				System.err.println(ioe);
-			}finally{
-			}
-	}//End-of-main
-}//End-ofclass
+			// sendMsg.start();
+
+		} catch (IOException ioe) {
+			System.err.println(ioe);
+		} finally {
+		}
+	}// End-of-main
+}// End-ofclass
--- Server.java
+++ Server.java
@@ -1,137 +1,138 @@
-/*
-course number (CS4345)
-Semester/Year (Spring 2019)
-assignment identifier (Assignment 3)
-all group members’ names (Wei Dong, Guanghui Li, Marco Colasito) 
-*/
 
+/*
+Course Number: CS4345
+Spring 2019
+Assignment 3
+Members: Wei Dong, Guanghui Li, Marco Colasito
+*/
 
 import java.net.*;
 import java.io.*;
 import java.util.*;
 
-
-public class Server{
+public class Server {
 	/*
-	create two set that store the user name and all the message user send to the server
-	*/
+	 * create two sets that store the usernames and all the messages that user send
+	 * to the server
+	 */
 	private static Set<String> allClient = new HashSet<>();
 	private static Set<DataOutputStream> writers = new HashSet<>();
 	public static boolean moreThenTwo = false;
-	public static void main(String[] args){
+
+	public static void main(String[] args) {
 		ServerSocket servSock;
 		Socket sock;
-		try{
+		try {
 			/*
-			each client has it own thread
-			*/
+			 * each client has its own thread
+			 */
 			servSock = new ServerSocket(7000);
-			System.out.println("Server started at "+ new Date() + '\n');
-			while(true){
-				//Listen for a connection request
+			System.out.println("Server started at " + new Date() + '\n');
+			while (true) {
+				// Listen for a connection request
 				sock = servSock.accept();
 				ChatRoom room = new ChatRoom(sock);
 				Thread clinet = new Thread(room);
 				clinet.start();
 			}
-		 } catch(IOException ioe){
-				System.err.println(ioe);
-			}
+		} catch (IOException ioe) {
+			System.err.println(ioe);
+		}
 
-	}//End-of-main
+	}// End-of-main
 
 	/*
-	thread class
-	*/
-	private static class ChatRoom implements Runnable{
-		
+	 * thread class
+	 */
+	private static class ChatRoom implements Runnable {
+
 		Socket socket;
 		String name;
 		DataOutputStream output2client;
 		DataInputStream input4mclient;
 
-		//pass down the client socket and assign it to local socket
-		public ChatRoom (Socket socket){
+		// pass down the client socket and assign it to local socket
+		public ChatRoom(Socket socket) {
 			this.socket = socket;
 		}
 
-		public void run(){
-			try{
-				//create data input and data output streams
+		public void run() {
+			try {
+				// create data input and data output streams
 				input4mclient = new DataInputStream(socket.getInputStream());
 				output2client = new DataOutputStream(socket.getOutputStream());
 
-			/*
-			Ask the user name and if user name are ready exist ask for another user name
-			*/
-			while(true){
-				output2client.writeUTF("YOURNAME");
-				name = input4mclient.readUTF();
-				if(name==null)
-					return;
-				//synchronized- let add new client to the group
-				synchronized (allClient){
-					if(name != null && !allClient.contains(name)){
-						allClient.add(name);
+				/*
+				 * Ask for the user name and if user name already exists ask for another user
+				 * name
+				 */
+				while (true) {
+					output2client.writeUTF("YOURNAME");
+					name = input4mclient.readUTF();
+					if (name == null)
+						return;
+					// synchronized- let add new client to the group
+					synchronized (allClient) {
+						if (name != null && !allClient.contains(name)) {
+							allClient.add(name);
+							break;
+						}
+					}
+				}
+
+				// broadcasts to everyone in the group chat that a new user has entered the chat
+				for (DataOutputStream w : writers) {
+					w.writeUTF("JOINGROUP" + name);
+				}
+
+				// stores the new client out put stream so we can boardcast to everyone
+				writers.add(output2client);
+
+				output2client.writeUTF("GOCHAT!");
+
+				// conversation starts here, once someone types LOGOUT he/she is going to sign
+				// out
+				while (true) {
+					String message = input4mclient.readUTF();
+					if (message.equals("LOGOUT")) {
+						output2client.writeUTF("YOUHAVELOGOUT");
 						break;
 					}
-				}
-			}
+					for (DataOutputStream w : writers) {
 
-			
-			//boardcast to everyone in the group chat that a new user has enter the chat
-			for(DataOutputStream w:  writers){
-				w.writeUTF("JOINGROUP" + name);
-			}
-			
-			//store the new client out put stream so we can boardcast to everyone
-			writers.add(output2client);
-
-			output2client.writeUTF("GOCHAT!");
-
-			//conversition start here, once someon type LOGOUT he/she is going to sign out
-			while(true){
-				String message = input4mclient.readUTF();
-				if(message.equals("LOGOUT")){
-					output2client.writeUTF("YOUHAVELOGOUT");
-					break;
-				}
-				for(DataOutputStream w : writers){
-					
-					w.writeUTF("Message" + name +":"+ message);
-				}
-			}//end of while sending message
-			System.out.println("123");
-
-		}catch(Exception e){
-			System.out.println(e);
-		}finally{
-			
-			/*
-			boardcase when someone had leave the chat
-			remove client from the list
-			*/
-			try {
-				System.out.println(name+" someone has left");
-
-				if(output2client != null){
-					writers.remove(output2client);
-				}
-				if(name != null){
-					allClient.remove(name);
-					for(DataOutputStream w : writers){
-						w.writeUTF("Message" + name +":"+ "has left the chat");
+						w.writeUTF("Message" + name + ": " + message + "\n");
 					}
-				}
+				} // end of while sending message
+				System.out.println("123");
 
-				//close the socket
-				socket.close();
-				
-			}catch(Exception e){
-				
+			} catch (Exception e) {
+				System.out.println(e);
+			} finally {
+
+				/*
+				 * base case when someone leaves the chat removes the client from the list
+				 */
+				try {
+					System.out.println(name + " has left\n");
+
+					if (output2client != null) {
+						writers.remove(output2client);
+					}
+					if (name != null) {
+						allClient.remove(name);
+						for (DataOutputStream w : writers) {
+							w.writeUTF("Message" + name + ": " + "has left the chat\n");
+						}
+					}
+
+					// close the socket
+					socket.close();
+
+				} catch (Exception e) {
+
+				}
 			}
-		}
 		}
 	}
 
-}//End-of-class
+}// End-of-class
